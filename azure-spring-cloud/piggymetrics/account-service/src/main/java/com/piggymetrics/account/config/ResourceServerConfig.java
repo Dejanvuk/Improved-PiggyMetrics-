490cfdb4b4ad4ea19c373d3a651af5cd07f7c20f
Dejan Vukovic (Lugera A-dvukovic Makler Romania) <a-dvukovic@microsoft.com>
2022-01-31 00:07:22 +0200

    added / to the configure method of resourceconfigserver in account service

--- azure-spring-cloud/piggymetrics/account-service/src/main/java/com/piggymetrics/account/config/ResourceServerConfig.java
+++ azure-spring-cloud/piggymetrics/account-service/src/main/java/com/piggymetrics/account/config/ResourceServerConfig.java
@@ -54,7 +54,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/" , "/demo", "actuator/**", "accounts/actuator/**").permitAll()
+                .antMatchers("/" , "/demo", "/actuator/**").permitAll()
                 .anyRequest().authenticated();
     }
 }
